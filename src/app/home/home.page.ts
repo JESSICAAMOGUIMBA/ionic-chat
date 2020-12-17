b135ad39a98e68842029cdc307599c8040702280
JESSICAAMOGUIMBA <jessica.amoguimba@epn.edu.ec>
2020-12-17 00:09:57 -0500

    Chat

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+// home.page.ts
+import { Component, ViewChild } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+
+interface StudentData {
+  Name: string;
+  Message: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +15,68 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('content') content: any;
+  studentList = [];
+  studentData: StudentData;
+  studentForm: FormGroup;
 
-  constructor() {}
+  constructor(
+    private firebaseService: FirebaseService,
+    public fb: FormBuilder,
+    public navCtrl: NavController
+  ) {
+    this.studentData = {} as StudentData;
+  }
+
+  ngOnInit() {
+    this.firebaseService.read_students().subscribe(data => {
+      console.log("Al inicio", this.studentList);
+      this.studentList = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          Name: e.payload.doc.data()['Name'],
+          Message: e.payload.doc.data()['Message'],
+        };
+      })
+      console.log("Al finalizar inicio", this.studentList);
+
+
+
+    });
+  }
+
+  CreateRecord() {
+    console.log("Que se va a enviar", this.studentData)
+    this.firebaseService.create_student(this.studentData)
+      .then(resp => {
+        this.studentData.Message = null;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    var poss = document.getElementById("final-scroll").offsetTop;
+    this.content.scrollTo(0, poss, 300);
+  }
+
+  RemoveRecord(rowID) {
+    this.firebaseService.delete_student(rowID);
+  }
+
+  EditRecord(record) {
+    record.isEdit = true;
+    record.EditName = record.Name;
+    record.EditAge = record.Age;
+    record.EditAddress = record.Address;
+  }
+
+  UpdateRecord(recordRow) {
+    let record = {};
+    record['Name'] = recordRow.EditName;
+    record['Age'] = recordRow.EditAge;
+    record['Address'] = recordRow.EditAddress;
+    this.firebaseService.update_student(recordRow.id, record);
+    recordRow.isEdit = false;
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDBDr98NSnLuuM4LKPxJRY3OFNg-3Bw-rU",
+    authDomain: "crud2020b-a979d.firebaseapp.com",
+    databaseURL: "https://crud2020b-a979d-default-rtdb.firebaseio.com",
+    projectId: "crud2020b-a979d",
+    storageBucket: "crud2020b-a979d.appspot.com",
+    messagingSenderId: "685081090045",
+    appId: "1:685081090045:web:dddd55a525c107f7dd2b94",
+    measurementId: "G-5XJ7NDSZS4"
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
